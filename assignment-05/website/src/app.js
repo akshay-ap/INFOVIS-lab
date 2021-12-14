75762dde14faa3122fe1ed85d763fe57824afdf4
Practical Course Information Visualization <pciv-tutors@visus.uni-stuttgart.de>
2021-12-14 13:51:49 +0100

    Skeleton assignment-05

---
+++ assignment-05/website/src/app.js
@@ -0,0 +1,5 @@
+document.getElementById("name").innerText = "{insert your name here}";;
+document.getElementById("assignment_number").innerText = "{insert the assignment number here}";
+/////////////////////////////////////////////////////
+import * as d3 from "d3";
+
