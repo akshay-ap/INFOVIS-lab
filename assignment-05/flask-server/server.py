d42812c65f9b592041acebd2313cb76f4ed15035
Akshay <akshay.ap95@gmail.com>
2022-01-09 14:27:16 +0100

    Assignment 5

--- assignment-05/flask-server/server.py
+++ assignment-05/flask-server/server.py
@@ -1,12 +1,22 @@
 from flask import Flask, Response, jsonify
 import pandas as pd
 import sqlite3
+import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/api/v1/get_data/", methods=["GET"])
-def main():
-    return Response(None)
+@app.route("/api/v1/get-data/authorkeywords-year", methods=["GET"])
+def authorkeywords_year_data():
+    with open('authorKeywords-year.json') as f:
+        data = json.load(f)
+        return jsonify(data)
+
+@app.route("/api/v1/get-data/abstract-conference", methods=["GET"])
+def data():
+    with open('abstract-conference.json') as f:
+        data = json.load(f)
+        return jsonify(data)
 
 app.run(port=8080)
+
